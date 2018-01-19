06e27c118ff2781f6188cfa73840d2dfeb2dd53a
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-01-18 20:37:59 -0600

    Part 1 Tables

---
+++ js/mesas.js
@@ -0,0 +1,98 @@
+<script>
+        
+        // Lienzo SVG donde serán creados todos los objetos
+        var lienzo = SVG('lienzo').size('100%', '140%');
+
+        // Representa las mesas y las sillas del salón con sus atributos
+        var mesa1 = [{
+            color: '#550544',
+            posicion: {
+                x: 70,
+                y: 140
+            },
+            sillas: [{
+                    numero: 1,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 50,
+                        y: 140
+                    }
+                }, {
+                    numero: 2,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 90,
+                        y: 115
+                    }
+                }, {
+                    numero: 3,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 140,
+                        y: 115
+                    }
+                }, {
+                    numero: 4,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 170,
+                        y: 150
+                    }
+                }, {
+                    numero: 5,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 180,
+                        y: 190
+                    }
+                }, {
+                    numero: 6,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 165,
+                        y: 220
+                    }
+                }, {
+                    numero: 7,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 130,
+                        y: 245
+                    }
+                }, {
+                    numero: 8,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 90,
+                        y: 245
+                    }
+                }, {
+                    numero: 9,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 60,
+                        y: 230
+                    }
+                }, {
+                    numero: 10,
+                    color: '#30f6ff',
+                    ocupada: false,
+                    posicion: {
+                        x: 40,
+                        y: 190
+                    }
+                }
+
+
+            ]
+        }]
+        </script>
