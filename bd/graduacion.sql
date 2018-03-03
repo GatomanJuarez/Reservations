db5b6ec19a52ba42cda96e89886f42d2505ef883
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-03-02 18:47:53 -0600

    BD?

--- bd/graduacion.sql
+++ bd/graduacion.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 26-01-2018 a las 14:52:23
--- Versión del servidor: 5.7.19
--- Versión de PHP: 5.6.31
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-03-2018 a las 00:46:42
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,14 +26,11 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `admins`
 --
 
-DROP TABLE IF EXISTS `admins`;
-CREATE TABLE IF NOT EXISTS `admins` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admins` (
+  `id` int(10) UNSIGNED NOT NULL,
   `admin` varchar(20) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
-  `clave` varchar(20) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `clave` (`clave`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+  `clave` varchar(20) COLLATE utf8_general_mysql500_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 --
 -- Volcado de datos para la tabla `admins`
@@ -50,27 +45,47 @@ INSERT INTO `admins` (`id`, `admin`, `clave`) VALUES
 -- Estructura de tabla para la tabla `alumnos`
 --
 
-DROP TABLE IF EXISTS `alumnos`;
-CREATE TABLE IF NOT EXISTS `alumnos` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `alumnos` (
+  `id` int(10) UNSIGNED NOT NULL,
   `nombre` varchar(20) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
   `paterno` varchar(20) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
   `materno` varchar(20) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
   `clave` varchar(20) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
   `sillas` int(6) DEFAULT NULL,
-  `pagado` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `clave` (`clave`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+  `pagado` tinyint(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 --
 -- Volcado de datos para la tabla `alumnos`
 --
 
 INSERT INTO `alumnos` (`id`, `nombre`, `paterno`, `materno`, `clave`, `sillas`, `pagado`) VALUES
-(1, 'Jesús Daniel', 'Pérez', 'Garcí­a', 'Danman 07', 7, 1),
+(1, 'Jesús Daniel', 'Pérez', 'Garcí­a', 'Danman 07', 5, 1),
 (2, 'Jesús Antonio', 'Juárez', 'Dévora', 'Filete 97', 5, 1),
-(3, 'José Ángel', 'Pérez', 'García', 'P Ludo', 3, 0);
+(3, 'José Ángel', 'Pérez', 'García', 'P Ludo', 3, 0),
+(4, 'asd', 'ads', 'das', 'ads', 2, 1),
+(5, 'wewe', 'qeeqw', 'qe', 'eqw', 3, 0),
+(6, '321', '312', '132', 'qdw', 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservadas`
+--
+
+CREATE TABLE `reservadas` (
+  `id` int(11) NOT NULL,
+  `reservada` int(5) NOT NULL,
+  `alumno` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `reservadas`
+--
+
+INSERT INTO `reservadas` (`id`, `reservada`, `alumno`) VALUES
+(2, 132, 1),
+(3, 75, 1);
 
 -- --------------------------------------------------------
 
@@ -78,15 +93,272 @@ INSERT INTO `alumnos` (`id`, `nombre`, `paterno`, `materno`, `clave`, `sillas`, 
 -- Estructura de tabla para la tabla `sillas`
 --
 
-DROP TABLE IF EXISTS `sillas`;
-CREATE TABLE IF NOT EXISTS `sillas` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `alumno_id` int(10) UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `alumno_id` (`alumno_id`)
+CREATE TABLE `sillas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `alumno_id` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
-COMMIT;
 
+--
+-- Volcado de datos para la tabla `sillas`
+--
+
+INSERT INTO `sillas` (`id`, `alumno_id`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0),
+(15, 0),
+(16, 0),
+(17, 0),
+(18, 0),
+(19, 0),
+(20, 0),
+(21, 0),
+(22, 0),
+(23, 0),
+(24, 0),
+(25, 0),
+(26, 0),
+(27, 0),
+(28, 0),
+(29, 0),
+(30, 0),
+(31, 0),
+(32, 0),
+(33, 0),
+(34, 0),
+(35, 0),
+(36, 0),
+(37, 0),
+(38, 0),
+(39, 0),
+(40, 0),
+(41, 0),
+(42, 0),
+(43, 0),
+(44, 0),
+(45, 0),
+(46, 0),
+(47, 0),
+(48, 0),
+(49, 0),
+(50, 0),
+(51, 0),
+(52, 0),
+(53, 0),
+(54, 0),
+(55, 0),
+(56, 0),
+(57, 0),
+(58, 0),
+(59, 0),
+(60, 0),
+(61, 0),
+(62, 0),
+(63, 0),
+(64, 0),
+(65, 0),
+(66, 0),
+(67, 0),
+(68, 0),
+(69, 0),
+(70, 0),
+(71, 0),
+(72, 0),
+(73, 0),
+(74, 0),
+(75, 0),
+(76, 0),
+(77, 0),
+(78, 0),
+(79, 0),
+(80, 0),
+(81, 0),
+(82, 0),
+(83, 0),
+(84, 0),
+(85, 0),
+(86, 0),
+(87, 0),
+(88, 0),
+(89, 0),
+(90, 0),
+(91, 0),
+(92, 0),
+(93, 0),
+(94, 0),
+(95, 0),
+(96, 0),
+(97, 0),
+(98, 0),
+(99, 0),
+(100, 0),
+(101, 0),
+(102, 0),
+(103, 0),
+(104, 0),
+(105, 0),
+(106, 0),
+(107, 0),
+(108, 0),
+(109, 0),
+(110, 0),
+(111, 0),
+(112, 0),
+(113, 0),
+(114, 0),
+(115, 0),
+(116, 0),
+(117, 0),
+(118, 0),
+(119, 0),
+(120, 0),
+(121, 0),
+(122, 0),
+(123, 0),
+(124, 0),
+(125, 0),
+(126, 0),
+(127, 0),
+(128, 0),
+(129, 0),
+(130, 0),
+(131, 0),
+(132, 1),
+(133, 0),
+(134, 0),
+(135, 1),
+(136, 0),
+(137, 0),
+(138, 0),
+(139, 1),
+(140, 0),
+(141, 0),
+(142, 0),
+(143, 0),
+(144, 0),
+(145, 0),
+(146, 0),
+(147, 0),
+(148, 0),
+(149, 0),
+(150, 0),
+(151, 0),
+(152, 0),
+(153, 0),
+(154, 0),
+(155, 0),
+(156, 0),
+(157, 0),
+(158, 0),
+(159, 0),
+(160, 0),
+(161, 0),
+(162, 0),
+(163, 0),
+(164, 0),
+(165, 0),
+(166, 0),
+(167, 0),
+(168, 0),
+(169, 0),
+(170, 0),
+(171, 0),
+(172, 0),
+(173, 0),
+(174, 0),
+(175, 0),
+(176, 0),
+(177, 0),
+(178, 0),
+(179, 0),
+(180, 0),
+(181, 0),
+(182, 0),
+(183, 0),
+(184, 0),
+(185, 0),
+(186, 0),
+(187, 0),
+(188, 0),
+(189, 0),
+(190, 0),
+(191, 0),
+(192, 0),
+(193, 0),
+(194, 0),
+(195, 0),
+(196, 0),
+(197, 0),
+(198, 0),
+(199, 0),
+(200, 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `clave` (`clave`);
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `clave` (`clave`);
+
+--
+-- Indices de la tabla `reservadas`
+--
+ALTER TABLE `reservadas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sillas`
+--
+ALTER TABLE `sillas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `alumno_id` (`alumno_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `reservadas`
+--
+ALTER TABLE `reservadas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `sillas`
+--
+ALTER TABLE `sillas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=201;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
